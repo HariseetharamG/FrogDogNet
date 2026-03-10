9ddc5d1ad2b18a7af27763ab84e87d4e1870e9fa
Gunduboina Hariseetharam <60320709+HariseetharamG@users.noreply.github.com>
2026-03-10 23:11:50 +0530

    Add files via upload

---
+++ scripts/FrogDogNet/domaingen_train.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+TRAINER=FrogDogNet
+# TRAINER=CoOp
+
+DATASET=$1
+SEED=$2
+
+# Define mapping between dataset argument names and actual folder names
+declare -A DATASET_PATHS
+DATASET_PATHS["resisc45"]="/Replace with your dataset location/RESISC45"
+DATASET_PATHS["patternnet"]="/Replace with your dataset location/PatternNet"
+DATASET_PATHS["mlrsnet"]="/Replace with your dataset location/MLRSNet"
+DATASET_PATHS["rsicd"]="/Replace with your dataset location/RSICD"
+
+# Get the correct data path based on the dataset argument
+DATA=${DATASET_PATHS[$DATASET]}
+
+# Check if the DATA path was found
+if [ -z "$DATA" ]; then
+  echo "Error: Dataset '$DATASET' not recognized. Please check the dataset name."
+  exit 1
+fi
+
+# Print the configuration for verification
+echo "Running with DATA=$DATA, TRAINER=$TRAINER, DATASET=$DATASET, SEED=$SEED"
+CFG=vit_b16_c4_ep10_batch1_ctxv1
+SHOTS=16
+SUB=all
+
+
+DIR=outputs/domain_generalization/${DATASET}/${TRAINER}/${CFG}_shots${SHOTS}/seed${SEED}
+if [ -d "$DIR" ]; then
+    echo "The results already exist in ${DIR}"
+else
+    python train.py \
+    --root ${DATA} \
+    --seed ${SEED} \
+    --trainer ${TRAINER} \
+    --dataset-config-file yaml/datasets/${DATASET}.yaml \
+    --config-file yaml/trainers/${TRAINER}/${CFG}.yaml \
+    --output-dir ${DIR} \
+    DATASET.NUM_SHOTS ${SHOTS} \
+    DATASET.SUBSAMPLE_CLASSES ${SUB}
+fi
